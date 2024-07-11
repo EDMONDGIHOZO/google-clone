88840511f19a7f7cbd2c2b473b17b302e8da2c84
edmond gihozo <34583961+EDMONDGIHOZO@users.noreply.github.com>
2024-07-11 02:54:29 +0200

    added url for search

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,3 @@
-
-
 export const randomImages = 'https://xsgames.co/randomusers/avatar.php?g=male';
-export const geoInfo = "https://get.geojs.io/v1/ip/geo.json"
+export const geoInfo = "https://get.geojs.io/v1/ip/geo.json";
+export const searchUrl = "https://api.artic.edu/api/v1/artworks/search?q=";
